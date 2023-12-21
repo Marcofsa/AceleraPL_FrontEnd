b2d2865eb3f1035af4517881011c419146446d20
Marcos Antonio Alves de Sá Filho <garenmanjador@hotmail.com>
2023-12-20 21:14:10 -0300

    First commit - Angular app

---
+++ AceleraPL_FrontEnd/Angular_App/Angular_app/src/app/components/primeiro/primeiro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-primeiro',
+  templateUrl: './primeiro.component.html',
+  styleUrls: ['./primeiro.component.scss']
+})
+export class PrimeiroComponent {
+
+}
